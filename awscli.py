9ed7fb234a4d8b744e252796aa891cfd00671e2f
Charan Reddy <charanreddy@Charans-MacBook-Air.local>
2020-02-23 13:17:48 +0530

    adding new file

--- awscli.py
+++ awscli.py
@@ -1,5 +1,6 @@
 import boto3
 import time
+import pprint
 
 AWS_ACCESS_KEY_ID = "AKIA3HNPFT27M5BCNN5N"
 AWS_SECRET_ACCESS_KEY = "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"
@@ -14,12 +15,20 @@ def printvm(id):
 def main():
     session = boto3.Session(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name='us-east-2')
     
-    ec2 = session.resource('ec2', region_name='us-east-2')
-    instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running','stopped']}])
-    #for instance in instances:
-            #dict[instance.id] = {'name': 'test', 'id':instance.id, 'state':instance.state}     
+    #ec2 = session.resource('ec2', region_name='us-east-2')
+    #ec2_re=session.resource(service_name= "ec2")  
+    ec2 =session.client(service_name="ec2")
+    
+    awsdict = ec2.describe_instances();
+    
+    for each_in in awsdict['Reservations']:
+        print("AWS instance : ",each_in)
+        pprint.pprint(each_in)
+        #print(each_in.id,each_in.state['Name'])
+        
+    '''' 
     printallvms()
     print ("running inifinity loop")
  
@@ -28,6 +37,7 @@ def main():
         
     
         for instance in instances:
+            print ("AWS instance)
             vm = dict.get(instance.id)
            # print("inside for loop")
             #print ('AWS', vm)
@@ -46,7 +56,7 @@ def main():
         time.sleep(5)
         
         
- 
+''' 
         
         
 if __name__ == '__main__':
